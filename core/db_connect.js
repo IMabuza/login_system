f8819fffa946d2230190e5bd2a279100f0de8c64
Innocent Mabuza <41034704+IMabuza@users.noreply.github.com>
2019-11-22 12:59:54 +0200

    return http status

--- core/db_connect.js
+++ core/db_connect.js
@@ -1,26 +1,14 @@
-const util = require('util');
-const mysql = require('mysql');
+const db_connect = require('mysql');
 
-const db_connect = mysql.createConnection({
-    connectionLimit: 10,
-    host:"localhost",
-    user:"user",
-    password:"pass",
-    database:"db"
+const connection = db_connect.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'yellow2019',
+  database: 'db'
+});
+connection.connect((err) => {
+  if (err) throw err;
+  console.log('Connected!');
 });
 
-db_coonect.getConnection((err, connection) => {
-    if(err){
-        console.error("Could not connect to the database");
-    
-    }
-
-    if(connection){
-        connection.release();
-        return;
-    }
-});
-
-db_connect.query = util.promisify(db_connect.query);
-
-module.exports = db_connect;
+module.exports = connection;
